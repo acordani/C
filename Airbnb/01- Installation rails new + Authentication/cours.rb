b8d539758e1885d400ad899a462b7a494c1550b1
Alex Cordani <alcordani@gmail.com>
2015-09-23 09:03:32 +0200

    Update cours.rb

---
+++ Airbnb/01- Installation rails new + Authentication/cours.rb
@@ -0,0 +1,25 @@
+On utilise soit l'option Minimal soit l'option avec Devise :
+
+## Minimal
+
+Get a minimal rails app ready to be deployed on Heroku with Bootstrap, Simple form and
+debugging gems.
+
+```bash
+rails new \
+  -T --database postgresql \
+  -m https://raw.githubusercontent.com/lewagon/rails-templates/master/minimal.rb \
+  CHANGE_THIS_TO_YOUR_RAILS_APP_NAME
+```
+
+## Devise
+
+Same as minimal **plus** a Devise install with a generated `User` model.
+
+
+```bash
+rails new \
+  -T --database postgresql \
+  -m https://raw.githubusercontent.com/lewagon/rails-templates/master/devise.rb \
+  CHANGE_THIS_TO_YOUR_RAILS_APP_NAME
+```
